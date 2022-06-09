a2750eea3185c3b253b2e03250aaa96dbea23fb9
wenbin <wenbin.shi@gmail.com>
2022-06-09 15:42:55 +0800

    update height

--- cmd/main.go
+++ cmd/main.go
@@ -17,13 +17,17 @@ func main() {
 	c, err := jsonrpc.NewClient("http://172.17.0.1:8545")
 	handleErr(err)
 
-	for i := 59000; i < 62000; i++ {
+	for i := 60910; i < 60940; i++ {
 		block, err := c.Eth().GetBlockByNumber(ethgo.BlockNumber(i), true)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 		fmt.Printf("block height:%d, block time: %d, total transactions:%d\n",
 			i, block.Timestamp, len(block.Transactions))
+
+		if receipt, err := c.Eth().GetTransactionReceipt(block.Transactions[0].Hash); err != nil {
+			fmt.Printf("block height:%d, receipt result is %d\n", i, receipt.Status)
+		}
 	}
 }
 
