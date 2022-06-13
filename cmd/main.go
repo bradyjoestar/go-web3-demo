54b896b544dfec2bc09d3b493889d42c7c15581d
wenbin <wenbin.shi@gmail.com>
2022-06-13 16:26:44 +0800

    add simple transferTx

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/umbracle/ethgo"
 	"github.com/umbracle/ethgo/jsonrpc"
+	"github.com/umbracle/ethgo/testutil"
+	"github.com/umbracle/ethgo/wallet"
+	"math/big"
 	"time"
 )
 
@@ -14,35 +17,67 @@ var (
 
 // call a contract
 func main() {
-
+	//
 	c, err := jsonrpc.NewClient("http://172.17.0.1:8545")
 	handleErr(err)
 
-	for i := 60910; i < 60940; i++ {
-		block, err := c.Eth().GetBlockByNumber(ethgo.BlockNumber(i), true)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		fmt.Printf("block height:%d, block time: %d, total transactions:%d\n",
-			i, block.Timestamp, len(block.Transactions))
-		fmt.Println(time.Unix(int64(block.Timestamp), 0))
+	key, _ := importWallet()
+	fmt.Println(key.Address())
+	found, err := c.Eth().GetBalance(key.Address(), ethgo.Latest)
+	handleErr(err)
+	fmt.Println(found.String())
 
-		//if len(block.Transactions) != 0 {
-		//	successNumber := 0
-		//	for j := 0; j < len(block.Transactions); j++ {
-		//		if receipt, err := c.Eth().GetTransactionReceipt(block.Transactions[j].Hash); err == nil {
-		//			if receipt.Status == 1 {
-		//				successNumber++
-		//			}
-		//		}
-		//	}
-		//	fmt.Printf("block height:%d, success number is %d\n", i, successNumber)
-		//}
+	nonce, _ := c.Eth().GetNonce(key.Address(), ethgo.Latest)
+	fmt.Println(nonce)
+
+	txn := &ethgo.Transaction{
+		From:     key.Address(),
+		Nonce:    nonce,
+		GasPrice: testutil.DefaultGasPrice,
+		Gas:      testutil.DefaultGasLimit,
+		To:       &testutil.DummyAddr,
+		Value:    big.NewInt(10),
 	}
+
+	chainID, err := c.Eth().ChainID()
+	signer := wallet.NewEIP155Signer(chainID.Uint64())
+	txn, err = signer.SignTx(txn, key)
+	data, err := txn.MarshalRLPTo(nil)
+
+	hash, err := c.Eth().SendRawTransaction(data)
+	fmt.Println(hash)
+	fmt.Println(err)
+
+	_, err = WaitForReceipt(c, hash)
+
+	balance, err := c.Eth().GetBalance(testutil.DummyAddr, ethgo.Latest)
+	fmt.Println(balance)
 }
 
 func handleErr(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func WaitForReceipt(c *jsonrpc.Client, hash ethgo.Hash) (*ethgo.Receipt, error) {
+	var receipt *ethgo.Receipt
+	var count uint64
+	for {
+		err := c.Call("eth_getTransactionReceipt", &receipt, hash)
+		if err != nil {
+			if err.Error() != "not found" {
+				return nil, err
+			}
+		}
+		if receipt != nil {
+			break
+		}
+		if count > 100 {
+			return nil, fmt.Errorf("timeout")
+		}
+		time.Sleep(500 * time.Millisecond)
+		count++
+	}
+	return receipt, nil
 }
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/umbracle/ethgo"
+	"github.com/umbracle/ethgo/jsonrpc"
 	"os"
+	"time"
 )
 
 func readFile(path string) string {
@@ -10,4 +14,20 @@ func readFile(path string) string {
 		panic(err)
 	}
 	return string(content)
+}
+
+func QueryHeight(c *jsonrpc.Client) {
+	for i := 10; i < 100; i++ {
+		block, err := c.Eth().GetBlockByNumber(ethgo.BlockNumber(i), true)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Printf("block height:%d, block time: %d, total transactions:%d\n",
+			i, block.Timestamp, len(block.Transactions))
+		fmt.Println(time.Unix(int64(block.Timestamp), 0))
+	}
+}
+
+func transferValue(c *jsonrpc.Client) {
+
 }
