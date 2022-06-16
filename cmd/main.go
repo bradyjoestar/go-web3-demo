d9cc2b7efbae4fd6b59dbb2a5356c65b6c77a3c8
wenbin <wenbin.shi@gmail.com>
2022-06-16 15:53:05 +0800

    support sendBatchTx

--- cmd/main.go
+++ cmd/main.go
@@ -27,37 +27,48 @@ func main() {
 	handleErr(err)
 	fmt.Println(found.String())
 
+	balance, err := c.Eth().GetBalance(testutil.DummyAddr, ethgo.Latest)
+	fmt.Printf("before balance is: %d\n", balance)
+
 	nonce, _ := c.Eth().GetNonce(key.Address(), ethgo.Latest)
 	fmt.Println(nonce)
 
-	txn := &ethgo.Transaction{
-		From:     key.Address(),
-		Nonce:    nonce,
-		GasPrice: testutil.DefaultGasPrice,
-		Gas:      testutil.DefaultGasLimit,
-		To:       &testutil.DummyAddr,
-		Value:    big.NewInt(10),
-	}
-
-	chainID, err := c.Eth().ChainID()
-	signer := wallet.NewEIP155Signer(chainID.Uint64())
-	txn, err = signer.SignTx(txn, key)
-	data, err := txn.MarshalRLPTo(nil)
-
-	hash, err := c.Eth().SendRawTransaction(data)
-	fmt.Println(hash)
-	fmt.Println(err)
-
+	hash, err := sendBatchTx(key, c, nonce, 20)
 	_, err = WaitForReceipt(c, hash)
 
-	balance, err := c.Eth().GetBalance(testutil.DummyAddr, ethgo.Latest)
-	fmt.Println(balance)
+	balance, err = c.Eth().GetBalance(testutil.DummyAddr, ethgo.Latest)
+	fmt.Printf("after balance is: %d\n", balance)
 }
 
 func handleErr(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func sendBatchTx(key ethgo.Key, c *jsonrpc.Client, nonce uint64, number uint64) (ethgo.Hash, error) {
+	var i uint64
+	var hash ethgo.Hash
+	for i = nonce; i < nonce+number; i++ {
+		txn := &ethgo.Transaction{
+			From:     key.Address(),
+			Nonce:    i,
+			GasPrice: testutil.DefaultGasPrice,
+			Gas:      testutil.DefaultGasLimit,
+			To:       &testutil.DummyAddr,
+			Value:    big.NewInt(10),
+		}
+
+		chainID, err := c.Eth().ChainID()
+		handleErr(err)
+		signer := wallet.NewEIP155Signer(chainID.Uint64())
+		txn, err = signer.SignTx(txn, key)
+		data, err := txn.MarshalRLPTo(nil)
+
+		hash, err = c.Eth().SendRawTransaction(data)
+		fmt.Printf("send transaction hash:%s\n", hash)
+	}
+	return hash, nil
 }
 
 func WaitForReceipt(c *jsonrpc.Client, hash ethgo.Hash) (*ethgo.Receipt, error) {
