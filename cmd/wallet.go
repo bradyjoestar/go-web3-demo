0a432ce92e7571e9332e1f3d04a43a5b8a7627a9
wenbin <wenbin.shi@gmail.com>
2022-06-17 10:36:48 +0800

    remove unused func

--- cmd/wallet.go
+++ cmd/wallet.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/umbracle/ethgo"
-	"github.com/umbracle/ethgo/jsonrpc"
 	"github.com/umbracle/ethgo/wallet"
 )
-
-// call a contract
-func validatr() {
-	key, _ := importWallet()
-	fmt.Println(key.Address())
-
-	c, err := jsonrpc.NewClient("http://localhost:8545")
-	handleErr(err)
-	found, err := c.Eth().GetBalance(key.Address(), ethgo.Latest)
-	handleErr(err)
-
-	fmt.Println(found.String())
-}
 
 func importWallet() (ethgo.Key, error) {
 	key, err := wallet.NewJSONWalletFromFile(walletFile, password)
